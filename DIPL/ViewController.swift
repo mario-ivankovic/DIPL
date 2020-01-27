b5a4290ebf0ec59ccc23e4aba9655cebdc587f52
Mario Ivanković <mario.ivankovic8@gmail.com>
2020-01-27 09:16:33 +0100

    Initial Commit

---
+++ DIPL/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DIPL
+//
+//  Created by Mario Ivankovic on 27/01/2020.
+//  Copyright © 2020 Mario Ivankovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
