f3f2d718d42528fab1c5b93b76caa6d6daf0d3c2
Mario Ivanković <mario.ivankovic8@gmail.com>
2020-03-13 00:06:19 +0100

    CommentVC

---
+++ DIPL/Controller/Feed/CommentVC.swift
@@ -0,0 +1,42 @@
+//
+//  CommentVC.swift
+//  DIPL
+//
+//  Created by Mario Ivankovic on 12/03/2020.
+//  Copyright © 2020 Mario Ivankovic. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+private let reuseIdentifier = "CommentCell"
+
+class CommentVC: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
+    // MARK: - Properties
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Register cell class
+        collectionView?.register(CommentCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+    }
+    
+    // MARK: - UICollectionView
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: 60)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! CommentCell
+        
+        return cell
+        
+    }
+}
---
+++ DIPL/View/CommentCell.swift
@@ -0,0 +1,20 @@
+//
+//  CommentCell.swift
+//  DIPL
+//
+//  Created by Mario Ivankovic on 12/03/2020.
+//  Copyright © 2020 Mario Ivankovic. All rights reserved.
+//
+
+import UIKit
+
+class CommentCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
