4f92e7898c6c8e437033b1e969f3621e721bfad1
Mario Ivanković <mario.ivankovic8@gmail.com>
2020-04-07 12:28:22 +0200

    Home feed pagination, solving problem that posts dont load at once

--- DIPL/Controller/Feed/FeedVC.swift
+++ DIPL/Controller/Feed/FeedVC.swift
@@ -18,6 +18,7 @@ class FeedVC: UICollectionViewController, UICollectionViewDelegateFlowLayout, Fe
     var posts = [Post]()
     var viewSinglePost = false
     var post: Post?
+    var currentKey: String?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -57,6 +58,14 @@ class FeedVC: UICollectionViewController, UICollectionViewDelegateFlowLayout, Fe
     }
 
     // MARK: - UICollectionViewDataSource
+    
+    override func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        if posts.count > 4 {
+            if indexPath.item == posts.count - 1 {
+                fetchPosts()
+            }
+        }
+    }
 
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
@@ -103,6 +112,7 @@ class FeedVC: UICollectionViewController, UICollectionViewDelegateFlowLayout, Fe
         userProfileVC.user = post.user
         
         navigationController?.pushViewController(userProfileVC, animated: true)
+        
     }
     
     func handleOptionsTapped(for cell: FeedCell) {
@@ -290,23 +300,47 @@ class FeedVC: UICollectionViewController, UICollectionViewDelegateFlowLayout, Fe
         
         guard let currentUid = Auth.auth().currentUser?.uid else { return }
         
-        USER_FEED_REF.child(currentUid).observe(.childAdded) { (snapshot) in
+        if currentKey == nil {
             
-            let postId = snapshot.key
-            
-            Database.fetchPost(with: postId, completion: { (post) in
+            USER_FEED_REF.child(currentUid).queryLimited(toLast: 5).observeSingleEvent(of: .value, with: { (snapshot) in
                 
-                self.posts.append(post)
-                
-                self.posts.sort(by: { (post1, post2) -> Bool in
-                    return post1.creationDate > post2.creationDate
-                })
-                
-                // Stop refreshing
                 self.collectionView?.refreshControl?.endRefreshing()
                 
-                self.collectionView?.reloadData()
+                guard let first = snapshot.children.allObjects.first as? DataSnapshot else { return }
+                guard let allObjects = snapshot.children.allObjects as? [DataSnapshot] else { return }
+                
+                allObjects.forEach({ (snapshot) in
+                    let postId = snapshot.key
+                    self.fetchPost(withPostId: postId)
+                })
+                self.currentKey = first.key
             })
+        } else {
+            USER_FEED_REF.child(currentUid).queryOrderedByKey().queryEnding(atValue: self.currentKey).queryLimited(toLast: 6).observeSingleEvent(of: .value, with: { (snapshot) in
+                
+                guard let first = snapshot.children.allObjects.first as? DataSnapshot else { return }
+                guard let allObjects = snapshot.children.allObjects as? [DataSnapshot] else { return }
+                
+                allObjects.forEach({ (snapshot) in
+                    let postId = snapshot.key
+                    if postId != self.currentKey {
+                        self.fetchPost(withPostId: postId)
+                    }
+                })
+                self.currentKey = first.key
+            })
+        }
+    }
+    
+    func fetchPost(withPostId postId: String) {
+        
+        Database.fetchPost(with: postId) { (post) in
+            
+            self.posts.append(post)
+            self.posts.sort(by: { (post1, post2) -> Bool in
+                return post1.creationDate > post2.creationDate
+            })
+            self.collectionView?.reloadData()
         }
     }
 }
